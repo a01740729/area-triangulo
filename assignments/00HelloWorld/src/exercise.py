1b04a490aaa6038b1e92d26bea8e844df2d2e941
a01740729 <a01740729@itesm.mx>
2021-08-20 01:32:37 +0000

    area triangulo

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,17 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
     pass
+
+    a = int(input('Dame el lado a: '))
+    b = int(input('Dame el lado b: '))
+    c = int(input('Dame el lado c: '))
+
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+
+    print('El área es: '+str(area))
+
 
 if __name__=='__main__':
     main()
